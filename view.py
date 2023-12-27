8e9c976e4a6b68f5a3b7b90c34bdc8aad3e194c1
sandropernerstorfer <sandro.pernerstorfer@gmail.com>
2023-12-27 20:47:29 +0100

    comment removed

--- view.py
+++ view.py
@@ -12,7 +12,6 @@ def coloredText(color, text):
     else                  : color = "0"
     return "\033["+color+"m"+text+"\033[0m"
 
-# generator for the dots in "Scanning..."
 def dotGenerator():
     count = 1
     while True:
