4d410d4c642f259c5908c99d42800a75931bbbcf
sandropernerstorfer <sandro.pernerstorfer@gmail.com>
2023-12-27 20:48:50 +0100

    code cleanup in main

--- main.py
+++ main.py
@@ -4,29 +4,26 @@ from utils import *
 from view import *
 
 imagePaths = getImagePaths()
-
 latestScanRight = 0
 detections = []
 
-while(True):
-        
+while(True):        
     currScanRight = 0
-    
     for img in imagePaths:
         try:
             found = list(locateAllOnScreen(img, confidence = 0.9))
-            if(len(found) == 0): continue
+            if len(found) == 0: continue
             for box in found:
-                if(box[0] > currScanRight):
+                if box[0] > currScanRight:
                     currScanRight = box[0]
         except:
             pass
-    
-    if(currScanRight > latestScanRight):
-        sendStatus = sendEmail() #TODO-default=True
-        detections.append(DetectionEntry(strftime("%d-%m-%Y %H:%M:%S", localtime()), sendStatus))
+    if currScanRight > latestScanRight:
+        sendStatus = sendEmail()
+        detections.append(
+            DetectionEntry(strftime("%d-%m-%Y %H:%M:%S", localtime()), sendStatus)
+        )
     
     drawView(detections)
-    
     latestScanRight = currScanRight
     sleep(1)
