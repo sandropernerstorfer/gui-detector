c9960727db6de7eace78c5175f69c23d20b179a0
sandropernerstorfer <sandro.pernerstorfer@gmail.com>
2023-12-27 20:48:28 +0100

    image loading cycle added

--- utils.py
+++ utils.py
@@ -1,18 +1,21 @@
 from os import getcwd, listdir
+from sys import stdout
 
 def getImagePaths():
-    imgDir = getcwd() + "/img/"
-    files = listdir(imgDir)
-    paths = []
-    for file in files[:]:
-        if file.endswith(".png"):
-            paths.append(imgDir + file)   
-    if len(paths) > 0:
-        return paths
-    else:
-        print("Missing .png image samples in \"img\" folder")
-        # TODO handle case
-        exit()
+    while(True):
+        imgDir = getcwd() + "/img/"
+        files = listdir(imgDir)
+        paths = []
+        for file in files[:]:
+            if file.endswith(".png"):
+                paths.append(imgDir + file)
+        if len(paths) > 0:
+            break
+        else:
+            stdout.write('\x1b[1A') 
+            stdout.write('\x1b[2K')
+            input("Missing .png image samples in \"img\" folder. Add images and press enter.")
+    return paths
 
 def sendEmail():
     # err    = return False
