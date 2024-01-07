9995b64117090069d20c8871940bd2a05244f042
sandropernerstorfer <sandro.pernerstorfer@gmail.com>
2024-01-08 00:25:26 +0100

    replace hardcoded values with variables

--- build.ps1
+++ build.ps1
@@ -21,9 +21,9 @@ Set-Location $mainDir
 Rename-Item -Path "$distDir\main.exe" -NewName "$appName.exe"
 
 # create dir on desktop populate with /img & .exe
-New-Item -Path $deskDir -Name "trading-detector" -ItemType "directory"
-New-Item -Path "$deskDir\trading-detector\" -Name "img" -ItemType "directory"
-Copy-Item -Path "$distDir\$appName.exe" -Destination "$deskDir\trading-detector\"
+New-Item -Path $deskDir -Name $appName -ItemType "directory"
+New-Item -Path "$deskDir\$appName\" -Name "img" -ItemType "directory"
+Copy-Item -Path "$distDir\$appName.exe" -Destination "$deskDir\$appName\"
 
 # remove temp dir
 Get-ChildItem -Path $tempDir -Recurse | Remove-Item -force -recurse
